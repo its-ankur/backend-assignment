4b9eb1705b99620b153ae3211d9251107de2eb6d
its-ankur <ankuratul2007@gmail.com>
2023-09-14 19:51:29 +0530

    don't know just commiting

--- fsStructure.js
+++ fsStructure.js
@@ -1,49 +1,56 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir("E:\\backend assignment\\Chitkara", function (error) {
-    if (error) {
-        console.log(error);
-    }
-    console.log("Directory created successfully!");
-});
+// fs.mkdir("E:\\backend assignment\\Chitkara", function (error) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log("Directory created successfully!");
+// });
 
-fs.mkdir("E:\\backend assignment\\Chitkara\\Course", function (error) {
-    if (error) {
-        console.log(error);
-    }
-    console.log("Directory created successfully!");
-});
+// fs.mkdir("E:\\backend assignment\\Chitkara\\Course", function (error) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log("Directory created successfully!");
+// });
 
-fs.mkdir("E:\\backend assignment\\Chitkara\\Student", function (error) {
-    if(error){
-        console.log(error);
-    }
-    console.log("Directory created successfully!");
-});
+// fs.mkdir("E:\\backend assignment\\Chitkara\\Student", function (error) {
+//     if(error){
+//         console.log(error);
+//     }
+//     console.log("Directory created successfully!");
+// });
 
-fs.mkdir("E:\\backend assignment\\Chitkara\\Faculty", function (error) {
-    if(error){
-        console.log(error);
-    }
-    console.log("Directory created successfully!");
-});
+// fs.mkdir("E:\\backend assignment\\Chitkara\\Faculty", function (error) {
+//     if(error){
+//         console.log(error);
+//     }
+//     console.log("Directory created successfully!");
+// });
 
-fs.writeFile("E:\\backend assignment\\Chitkara\\Course\\Counselor.txt","Ankur", function (error) {
-    if (error) {
-        console.log(error);
-    }
-    console.log("File created successfully!");
-});
+// fs.writeFile("E:\\backend assignment\\Chitkara\\Course\\Counselor.txt","Ankur", function (error) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log("File created successfully!");
+// });
 
-fs.writeFile("E:\\backend assignment\\Chitkara\\Student\\StudentList.txt","Akhil", function (error) {
-    if (error) {
-        console.log(error);
-    }
-    console.log("File created successfully!");
-});
+// fs.writeFile("E:\\backend assignment\\Chitkara\\Student\\StudentList.txt","Akhil", function (error) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log("File created successfully!");
+// });
 
-fs.writeFile("E:\\backend assignment\\Chitkara\\Faculty\\FacultyList.txt","Ankush", function (error,) {
+// fs.writeFile("E:\\backend assignment\\Chitkara\\Faculty\\FacultyList.txt","Ankush", function (error,) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log("File created successfully!");
+// });
+
+fs.writeFile("E:\\backend assignment\\Chitkara\\Course\\Counselor.txt","Akash", function (error) {
     if (error) {
         console.log(error);
     }
